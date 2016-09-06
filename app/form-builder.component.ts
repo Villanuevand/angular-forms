12e0465793bf89292108db0c2b3f92495dd7b656
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-09-06 00:13:27 -0300

    Add: feature form builder

--- app/form-builder.component.ts
+++ app/form-builder.component.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 @Component({
-    template: `
-    <h2>Form Builder</h2>
-    <p>More complex way!</p>`
+    templateUrl: './app/templates/form-builder.component.html'
 })
-export class FormBuilderComponent { }
+export class FormBuilderComponent implements OnInit{
+
+    registerForm: FormGroup;
+    formValues: any;
+
+    constructor(private fb: FormBuilder) {};
+
+    ngOnInit() {
+        this.registerForm = this.fb.group({
+            user: this.fb.group({
+                firstname: ['Andres'],
+                lastname: ['Villanueva'],
+            }),
+            address: this.fb.group({
+                street: [],
+                zip: [],
+                city: []
+            })
+        });
+    }
+
+    submitForm () {
+        console.log('click submit!');
+        this.formValues = this.registerForm.value;
+    }
+}
